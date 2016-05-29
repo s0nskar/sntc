d4fab7443c12ae182c579f7d9cdc239a2f0b91aa
Sanskar Modi <sanskarmodi97@gmail.com>
2016-05-30 03:52:41 +0530

    Fixed collectstatic error and created views

--- sntc/urls.py
+++ sntc/urls.py
@@ -7,6 +7,13 @@ from website import views
 
 urlpatterns = [
     url(r'^$', 'website.views.home', name='home'),
+    url(r'^amc/$', 'website.views.amc', name='amc'),
+    url(r'^astro/$', 'website.views.astro', name='astro'),
+    url(r'^cops/$', 'website.views.cops', name='cops'),
+    url(r'^green/$', 'website.views.green', name='green'),
+    url(r'^out/$', 'website.views.out', name='out'),
+    url(r'^robo/$', 'website.views.robo', name='robo'),
+    url(r'^sae/$', 'website.views.sae', name='sae'),
 
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- website/views.py
+++ website/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'index.html', {})
+
+def amc(request):
+	return render(request, 'amc.html', {})
+
+def astro(request):
+	return render(request, 'astro.html', {})
+
+def cops(request):
+	return render(request, 'cops.html', {})
+
+def green(request):
+	return render(request, 'green.html', {})
+
+def out(request):
+	return render(request, 'out.html', {})
+
+def robo(request):
+	return render(request, 'robo.html', {})
+
+def sae(request):
+	return render(request, 'sae.html', {})
