b2e0dc7a16d99805c6aba65016d402fa5cf81ebe
Sanskar Modi <sanskarmodi97@gmail.com>
2016-06-04 19:18:55 +0530

    removed url deprecation error

--- sntc/urls.py
+++ sntc/urls.py
@@ -3,17 +3,17 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-from website import views
+from website import views as web_views
 
 urlpatterns = [
-    url(r'^$', 'website.views.home', name='home'),
-    url(r'^amc/$', 'website.views.amc', name='amc'),
-    url(r'^astro/$', 'website.views.astro', name='astro'),
-    url(r'^cops/$', 'website.views.cops', name='cops'),
-    url(r'^green/$', 'website.views.green', name='green'),
-    url(r'^out/$', 'website.views.out', name='out'),
-    url(r'^robo/$', 'website.views.robo', name='robo'),
-    url(r'^sae/$', 'website.views.sae', name='sae'),
+    url(r'^$', web_views.home, name='home'),
+    url(r'^amc/$', web_views.amc, name='amc'),
+    url(r'^astro/$', web_views.astro, name='astro'),
+    url(r'^cops/$', web_views.cops, name='cops'),
+    url(r'^green/$', web_views.green, name='green'),
+    url(r'^out/$', web_views.out, name='out'),
+    url(r'^robo/$', web_views.robo, name='robo'),
+    url(r'^sae/$', web_views.sae, name='sae'),
 
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
