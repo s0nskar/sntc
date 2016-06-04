feb51ec13b1d1e9c5993ed506c638b425f854b57
Sanskar Modi <sanskarmodi97@gmail.com>
2016-06-04 17:55:14 +0530

    added models

--- website/models.py
+++ website/models.py
@@ -2,4 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Club(models.Model):
+	name = models.CharField(max_length=30)
+	about = models.TextField()
+	vision = models.TextField()
+
+	def __unicode__(self):
+		return self.name
+
+class SubTopic(models.Model):
+	club = models.ForeignKey('Club')
+	heading = models.CharField(max_length=20)
+	about = models.TextField()
+
+	def __unicode__(self):
+		return self.club
+
+class Vision(models.Model):
+	name = models.CharField(max_length=30)
+	post = models.CharField(max_length=30)
+	text = models.TextField()
+
+	def __unicode__(self):
+		return self.name
